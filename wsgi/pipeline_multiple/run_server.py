a0e8adcb112d831283b0cca5d9b77ca9e52f30d3
Ahsanul08 <ahsanul.haque.ovi@gmail.com>
2015-07-08 17:20:55 +0600

    Deleted rubbish things

--- wsgi/pipeline_multiple/run_server.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'arun'
-
-if __name__ == '__main__':
-    from paste import httpserver
-    from paste.deploy import loadapp
-    httpserver.serve(loadapp('config:setup.cfg', relative_to='.'), host='127.0.0.1', port='8888')
-    print "Server "
